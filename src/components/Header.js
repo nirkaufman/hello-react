ca173ed9c6d21f22b3a4418220eb5b7d4690c564
Nir Kaufman <nirkaufman@gmail.com>
2018-10-30 08:56:01 -0400

    init

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+export class Header extends Component {
+
+  state = { todo: '' };
+
+  handleChange = ({ target }) => {
+    this.setState({ todo: target.value })
+  };
+
+  handleSubmit = ({ keyCode }) => {
+    if(keyCode === 13) {
+      console.log(this.state);
+    }
+  };
+
+  render() {
+    return <header className="header">
+      <h1>{this.props.title}</h1>
+      <input className="new-todo"
+             value={this.state.todo}
+             onChange={this.handleChange}
+             onKeyDown={this.handleSubmit}
+             placeholder="What needs to be done?"
+             autoFocus/>
+    </header>;
+  }
+}
